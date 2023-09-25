24ab1b30e906eaabbbc15fe3cc53f5df2993ba3d
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-09-25 20:14:24 +0530

    fixed api

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import uvicorn 
-
+import uvicorn
 from os import getenv
 
 if __name__ == "__main__":
-    port = int(getenv("PORT", 8000))
-    uvicorn.run("app.api:app" ,host = "0.0.0.0", port = port , reload = True)
+    default_port = 8000
+    port = int(getenv("PORT", default_port))
+
+    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
