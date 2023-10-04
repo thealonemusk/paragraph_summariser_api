a966fd60d751193c16ce5ff063577f067a3172ce
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-10-04 23:16:20 +0530

    remove port setting

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import uvicorn
-# from os import getenv
 
 if __name__ == "__main__":
-    # default_port = 8000
-    # port = int(getenv("PORT", 3000))
-
-    uvicorn.run("api:app", host="0.0.0.0", port=3000)
+    uvicorn.run("api:app", host="0.0.0.0")
