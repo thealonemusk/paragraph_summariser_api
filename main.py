88b0f71f90ddb74df0f4cac78f85f12b1376d172
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-10-04 23:05:57 +0530

    change port

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from os import getenv
 
 if __name__ == "__main__":
-    default_port = 8000
-    port = int(getenv("PORT", default_port))
+    # default_port = 8000
+    port = int(getenv("PORT", 8000))
 
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
