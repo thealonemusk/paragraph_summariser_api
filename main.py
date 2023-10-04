d6f672124476983eeb44838cfeaef5a769942425
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-10-04 23:06:41 +0530

    change port to 3000

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from os import getenv
 
 if __name__ == "__main__":
     # default_port = 8000
-    port = int(getenv("PORT", 8000))
+    port = int(getenv("PORT", 3000))
 
     uvicorn.run("api:app", host="0.0.0.0", port=port)
